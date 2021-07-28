6c3852950c6b04b640f1b348644de9b923a68a36
kleber <kleberguerra22@hotmail.com>
2021-07-27 23:52:33 -0500

    Crear Actores

--- src/actores/CrearActores.tsx
+++ src/actores/CrearActores.tsx
@@ -1,14 +1,40 @@
 /** @format */
 
+import axios from "axios";
+import { urlActores } from "../utils/endpoints";
+import { actorCreacionDTO } from "./actores.model";
 import FormularioActores from "./FormularioActores";
+import { useHistory } from "react-router-dom";
+import MostrarErrores from "../utils/MostrarErrores";
+import { useState } from "react";
+import { convertirActorAFormData } from "../utils/formDataUtils";
 
 export default function CrearActores() {
+  const [errores, setErrores] = useState<string[]>([]);
+  const history = useHistory();
+
+  async function crear(actor: actorCreacionDTO) {
+    try {
+      const formData = convertirActorAFormData(actor);
+      await axios({
+        method: "post",
+        url: urlActores,
+        data: formData,
+        headers: { "Content-type": "multipart/form-data" },
+      });
+      history.push("/actores");
+    } catch (error) {
+      setErrores(error.response.data);
+    }
+  }
+
   return (
     <>
       <h3>Crear Actores</h3>
+      <MostrarErrores errores={errores} />
       <FormularioActores
         modelo={{ nombre: "", fechaNacimiento: undefined }}
-        onSubmit={(valores) => console.log(valores)}
+        onSubmit={async (valores) => await crear(valores)}
       />
     </>
   );
--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -2,3 +2,4 @@
 console.log("valor por defecto", process.env.REACT_APP_API_URL);
 const apiURL = process.env.REACT_APP_API_URL;
 export const urlGeneros = `${apiURL}/generos`;
+export const urlActores = `${apiURL}/actores`;
